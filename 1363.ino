e039fd3827658f12617b30a2b0b447f78eeb9296
Peter S. C <ps.craik@gmail.com>
2016-06-13 16:42:47 +0100

    Button hex data as C const char arrays
    #include this in the main program. Names are of form B_[name of button in caps]

---
+++ 1363.ino
@@ -0,0 +1,32 @@
+/* 
+hex values for remote buttons, on the setting TV Acer 1363
+*/
+/* Play button not known, test soon */
+
+const char B_MUTE[2] =    {0xE8, 0x17};
+const char B_POWER[2] =   {0xF8, 0x7};
+const char B_1[2] =       {0x8, 0xF7};
+const char B_2[2] =       {0x88, 0x77};
+const char B_3[2] =       {0x48, 0xB7};
+const char B_4[2] =       {};
+const char B_5[2] =       {};
+const char B_6[2] =       {};
+const char B_7[2] =       {};
+const char B_8[2] =       {};
+const char B_9[2] =       {};
+const char B_0[2] =       {};
+const char B_AV[2] =      {};
+const char B_MENU[2] =    {};
+const char B_UP[2] =      {};
+const char B_DOWN[2] =    {};
+const char B_LEFT[2] =    {};
+const char B_RIGHT[2] =   {};
+const char B_PPLUS[2] =   {};
+const char B_PMINUS[2] =  {};
+const char B_VPLUS[2] =   {};
+const char B_VMINUS =     {};
+const char B_OK[2] =      {};
+const char B_BB[2] =      {};
+const char B_FF[2] =      {};
+const char B_REC[2] =     {};
+const char B_PLAY[2] =    {};
