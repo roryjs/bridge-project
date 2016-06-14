43eaa442cf3958044c50780ac1359e36a46811cd
Rory J Sanderson <rory@rjsanderson.com>
2016-06-14 16:46:13 +0100

    remove old code

--- Programming/LED-test/LED-test.ino
+++ Programming/LED-test/LED-test.ino
@@ -24,13 +24,7 @@ void loop() {
         Serial.print(B_MUTE[0], HEX);
         Serial.print(B_MUTE[1], HEX);
         Serial.println();
-        /*
-        if (output[0] == B_LEFT[0] && output[1] == B_LEFT[1]) {
-          digitalWrite(13, HIGH);
-        } else if (output[0] == B_RIGHT[0] && output[1] == B_RIGHT[1]) {
-          digitalWrite(13, LOW);
-        }
-        */
+
         switch(dta[8]) //first byte of button data is unique for each button!
         {
             case B_1[0]:
