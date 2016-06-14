094e271c57664cdddb8fe60d29570b7acb85462e
Rory J Sanderson <rory@rjsanderson.com>
2016-06-14 11:03:32 +0100

    add button list to LED test

--- Programming/LED-test/LED-test.ino
+++ Programming/LED-test/LED-test.ino
@@ -1,8 +1,7 @@
 #include <IRSendRev.h>
+#include "1363.cpp"
 
 const int pinRecv = 2;
-char B_MUTE[2] = {0xE8, 0x17};
-char POWER[2] = {0xF8, 0x7};
 
 void setup() {
     Serial.begin(115200);
@@ -17,7 +16,7 @@ void loop() {
     if(IR.IsDta()) {
         IR.Recv(dta);               // receive data to dta
 
-        char output[2] = {dta[8], dta[9]};
+        unsigned char output[2] = {dta[8], dta[9]};
 
         Serial.print(output[0], HEX);
         Serial.print(output[1], HEX);
@@ -26,9 +25,9 @@ void loop() {
         Serial.print(B_MUTE[1], HEX);
         Serial.println();
         
-        if (output[0] == B_MUTE[0] && output[1] == B_MUTE[1]) {
+        if (output[0] == B_LEFT[0] && output[1] == B_LEFT[1]) {
           digitalWrite(13, HIGH);
-        } else if (output[0] == POWER[0] && output[1] == POWER[1]) {
+        } else if (output[0] == B_RIGHT[0] && output[1] == B_RIGHT[1]) {
           digitalWrite(13, LOW);
         }
     }
