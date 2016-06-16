c0ce6f79f807cf5ecbe12eba933531b139b42265
Peter S. C <PSamuelC@users.noreply.github.com>
2016-06-16 10:23:25 +0100

    working version
    has the commands to set pins 1-7 as outputs this time!

--- Programming/display-test/display-test.ino
+++ Programming/display-test/display-test.ino
@@ -1,25 +1,53 @@
 #include <IRSendRev.h>
-#include "1363.cpp"
+#include "data1363.cpp"
 
-const int pinRecv = 2;
+const int pinRecv = 0;
 
 void setup() {
-    Serial.begin(115200);
+    //Serial.begin(115200);
     IR.Init(pinRecv);
-
+    
+    pinMode(0, INPUT);
+    pinMode(1, OUTPUT);
+    pinMode(2, OUTPUT);
+    pinMode(3, OUTPUT);
+    pinMode(4, OUTPUT);
+    pinMode(5, OUTPUT);
+    pinMode(6, OUTPUT);
+    pinMode(7, OUTPUT);
+    //pinMode(8, OUTPUT);
+    //pinMode(9, OUTPUT);
     pinMode(13, OUTPUT);
+
+    digitalWrite(13, HIGH);
+    PORTD = B00000000;
+    delay(1000);
+    PORTD = B11111110; //set pins 1 to 7 high, ie all segments OFF
 }
 
 unsigned char dta[20];
-int 
+int sevsegset(int num); // function prototype
+/* make an array of 8bit bytes
+   the nth element of the array contains the bits to set the pins to display the numeral n */
+byte digits[10] = {B10000000, \
+                  B11110010, \
+                  B01001000, \
+                  B01100000, \
+                  B00110010, \
+                  B00100100, \
+                  B00000100, \
+                  B11110000, \
+                  B00000000, \
+                  B00100000};
 
 void loop() {
+  
     if(IR.IsDta()) {
         IR.Recv(dta);               // receive data to dta
 
         switch(dta[8]) //first byte of button data is unique for each button!
         {
-            /* number keys */
+            // number keys
             case B_1[0]:      // if the "1" key is pressed...
                 sevsegset(1); // set 7 segment display to show a number 1
                 break;
@@ -51,13 +79,22 @@ void loop() {
                 sevsegset(0);
                 break;
 
-            /* other keys */
+            // other keys
             default:
-                // flash LED once
-                digitalWrite(13, HIGH);
-                delay(500);
-                digitalWrite(13, LOW);              
-                break;
+                //cycle through all numbers for 1 sec
+                for (int q = 0; q < 10; q++)
+                {
+                    PORTD = digits[q];
+                    delay(100); 
+                }
+                PORTD = B11111110;
         }
     }
+  
+
+}
+
+int sevsegset(int num)
+{
+    PORTD = digits[num];
 }
